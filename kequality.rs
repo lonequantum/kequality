8884a9662b5be5d596c5c8f65a6438d26680b4c7
lonequantum <lonequantum@lnode.net>
2020-09-29 13:14:01 +0200

    Replace all code

--- kequality.rs
+++ kequality.rs
@@ -1,135 +1,137 @@
-// SPOJ.com requires the solution to be one file only,
+// spoj.com requires the solution to be one file only,
 // so we are not using any external definition.
 
 
-// A kingdom is a set of trees
-// (there can be more than one tree if a road between two cities is closed).
 mod kingdom {
+    use std::collections::HashMap;
+
+    #[allow(non_camel_case_types)]
+    pub type size_k = usize;
+    pub type CityId = size_k;
+
+    // A road is a one-way link between two cities.
+    struct Road {
+        reachable_cities_count: size_k
+    }
 
     // A city is a node in a tree.
-    mod city {
-        use std::collections::HashMap;
+    struct City {
+        id: CityId, // convenient, but redundant with cities indexation at the kingdom level
+        roads_to: HashMap<CityId, Road>
+    }
 
-        pub type Id = u32; // for values between 1 and 200_000
-        pub type IdSized = u32; // for counting cities or roads - can't exceed the maximum Id
-
-        pub struct City {
-            //id: Id, // can be removed - redundant with cities indexation
-            roads_to: HashMap<Id, IdSized>, // links to other cities
+    impl City {
+        fn update_road_to(&mut self, destination: &City) {
+            self.roads_to.insert(
+                destination.id,
+                Road {
+                    reachable_cities_count: destination.count_reachable_cities(self.id) + 1
+                }
+            );
         }
 
-        impl City {
-            pub fn new() -> City {
-                City {
-                    roads_to: HashMap::new(),
+        fn count_reachable_cities(&self, exclude: CityId) -> size_k {
+            let mut count = 0;
+            for (&city_id, road) in &self.roads_to {
+                if city_id != exclude {
+                    count += road.reachable_cities_count
                 }
             }
 
-            // Adds a one-way road from our city to another city.
-            pub fn add_road_to(&mut self, other_city_id: Id) -> &mut City {
-                self.roads_to.entry(other_city_id).or_insert(0);
-
-                self
-            }
+            count
         }
     }
 
-
-    use kingdom::city::City;
-    pub use kingdom::city::Id as CityId;
-    pub use kingdom::city::IdSized as CityIdSized;
-    use std::collections::HashMap;
-
+    // A kingdom is a set of trees
     pub struct Kingdom {
-        cities: HashMap<CityId, City>, // indexes all nodes of all trees
+        cities: Vec<City> // indexes all nodes of all trees
     }
 
     impl Kingdom {
-        pub fn new() -> Kingdom {
+        pub fn new(number_of_cities: size_k) -> Kingdom {
+            let mut cities = Vec::new();
+            for i in 0..number_of_cities {
+                cities.push(
+                    City {
+                        id: i + 1,
+                        roads_to: HashMap::new()
+                    }
+                );
+            }
+
             Kingdom {
-                cities: HashMap::new(),
+                cities
             }
         }
 
-        // Adds a two-ways road between two cities
-        // (we automatically add cities if they don't exist).
-        pub fn add_road(&mut self, city_id_1: CityId, city_id_2: CityId) -> &mut Kingdom {
-            self.insert_city(city_id_1).add_road_to(city_id_2);
-            self.insert_city(city_id_2).add_road_to(city_id_1);
+        /*fn get_refmut_city(&mut self, city_id: CityId) -> &mut City {
+            &mut self.cities[city_id - 1]
+        }*/
 
-            self
+        // Adds a two-way link between two cities
+        pub fn add_roads(&mut self, city_id_1: CityId, city_id_2: CityId) {
+            /*let city_1 = self.get_refmut_city(city_id_1);
+            let city_2 = self.get_refmut_city(city_id_2);
+
+            city_1.update_road_to(city_2);*/
         }
 
-        pub fn add_city(&mut self, city_id: CityId) -> &mut Kingdom {
-            self.insert_city(city_id);
-
-            self
-        }
-
-        fn insert_city(&mut self, city_id: CityId) -> &mut City {
-            self.cities.entry(city_id).or_insert(City::new())
-        }
-
-        pub fn solve(&self, query: Vec<CityId>) -> CityIdSized {
+        pub fn solve(&self, query: Vec<CityId>) -> size_k {
             match query.len() {
                 1 => 1,
-                _ => 42,
+                _ => 42
             }
         }
     }
 }
 
 
-// Parses input data and solves each query.
 fn main() {
     use kingdom::*;
     use std::io;
 
-    let mut domki = Kingdom::new();
+    let number_of_cities: size_k = {
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
 
-    let number_of_cities: CityIdSized = {
-        let mut l = String::new();
-        io::stdin().read_line(&mut l).unwrap();
-
-        l.trim().parse().unwrap()
+        line.trim().parse().unwrap()
     };
 
-    for _ in 1..number_of_cities {
-        let mut l = String::new();
-        let mut road_definition = {
-            io::stdin().read_line(&mut l).unwrap();
+    let mut domki = Kingdom::new(number_of_cities);
 
-            l.split_whitespace()
-             .map(|v| v.parse().unwrap())
+    for _ in 1..number_of_cities {
+        let mut line = String::new();
+        let mut road_definition = {
+            io::stdin().read_line(&mut line).unwrap();
+
+            line.split_whitespace()
+                .map(|value| value.parse().unwrap())
         };
 
         let city_id_1: CityId = road_definition.next().unwrap();
         let city_id_2: CityId = road_definition.next().unwrap();
-
+        
         if let Some(1) = road_definition.next() {
-            domki.add_road(city_id_1, city_id_2);
-        } else {
-            domki.add_city(city_id_1)
-                 .add_city(city_id_2);
+            domki.add_roads(city_id_1, city_id_2);
         }
     }
 
-    let number_of_queries: u32 = { // between 1 and 200_000
-        let mut l = String::new();
-        io::stdin().read_line(&mut l).unwrap();
+    let number_of_queries: u32 = {
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
 
-        l.trim().parse().unwrap()
+        line.trim().parse().unwrap()
     };
 
     for _ in 0..number_of_queries {
         let query: Vec<CityId> = {
-            let mut l = String::new();
-            io::stdin().read_line(&mut l).unwrap();
+            let mut line = String::new();
+            io::stdin().read_line(&mut line).unwrap();
 
-            l.split_whitespace()
-             .skip(1) // first number on each line is useless
-             .map(|v| v.parse().unwrap())
-             .collect()
+            line.split_whitespace()
+                .skip(1) // first number on each line is useless
+                .map(|value| value.parse().unwrap())
+                .collect()
         };
 
         println!("{}", domki.solve(query));

06c85d70a20adfb887a4586c53c9c1d29a8651a7
lonequantum <lonequantum@lnode.net>
2020-10-13 10:25:38 +0200

    Capitalize constants

--- gen-test-perf.sh
+++ gen-test-perf.sh
@@ -1,43 +1,45 @@
 #!/bin/bash
 # Generates random input data for performance testing
 
-output_file="test-perf.txt"
+OUTPUT_FILE="test-perf.txt"
 
-number_of_cities=200000
-max_closed_roads=2
-echo $number_of_cities > "$output_file"
+NUMBER_OF_CITIES=200000
+TOTAL_PEOPLE_INIT=$(shuf -i 100000-200000 -n 1)
+MAX_CLOSED_ROADS=2
 
-echo -n "Generating $((number_of_cities - 1)) links… "
+echo $NUMBER_OF_CITIES > "$OUTPUT_FILE"
+
+echo -n "Generating $((NUMBER_OF_CITIES - 1)) links… "
 closed_roads=0
 
-echo "1 2 1" >> "$output_file"
-for city_id in $(seq 3 $number_of_cities); do
+echo "1 2 1" >> "$OUTPUT_FILE"
+for city_id in $(seq 3 $NUMBER_OF_CITIES); do
         limit=$((city_id - 1))
         start=$(shuf -i 1-${limit} -n 1)
         open=$RANDOM
-        if [ $open -lt 16 -a $closed_roads -lt $max_closed_roads ]; then
+        if [ $open -lt 16 -a $closed_roads -lt $MAX_CLOSED_ROADS ]; then
                 open=0
                 closed_roads=$((closed_roads + 1))
         else
                 open=1
         fi
-        echo "$start $city_id $open" >> "$output_file"
+        echo "$start $city_id $open" >> "$OUTPUT_FILE"
 done
 echo -e "\e[92mDone\e[0m"
 
 echo -n "Generating queries… "
 
-total_people=$(shuf -i 100000-200000 -n 1)
+total_people=$TOTAL_PEOPLE_INIT
 declare -a queries
 
 while [ $total_people -gt 0 ]; do
         people=$(shuf -i 1-${total_people} -n 1)
         cities=""
 
-        limit=$((people > number_of_cities ? number_of_cities : people))
+        limit=$((people > NUMBER_OF_CITIES ? NUMBER_OF_CITIES : people))
         length_of_query=$(shuf -i 1-${limit} -n 1)
         while [ $length_of_query -gt 0 ]; do
-                cities="$cities $(shuf -i 1-${number_of_cities} -n 1)"
+                cities="$cities $(shuf -i 1-${NUMBER_OF_CITIES} -n 1)"
                 length_of_query=$((length_of_query - 1))
         done
 
@@ -48,10 +50,10 @@ while [ $total_people -gt 0 ]; do
 done
 
 number_of_queries=${#queries[@]}
-echo $number_of_queries >> "$output_file"
+echo $number_of_queries >> "$OUTPUT_FILE"
 
 for i in "${queries[@]}"; do
-        echo "$i" >> "$output_file"
+        echo "$i" >> "$OUTPUT_FILE"
 done
 
 echo -e "\e[92m${number_of_queries}\e[0m"
